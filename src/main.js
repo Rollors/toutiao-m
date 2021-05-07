a2e0e408b7b1ded445425c9fe68a82f5dff74feb
Rollors <18270387673@163.com>
2021-05-07 15:04:55 +0800

    项目配置修正

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 移动端适配Vant中单位使用px，需要导入amfe-flexible，转换为rem
+import 'amfe-flexible'
+Vue.config.productionTip = false
+Vue.use(Vant)
 
-createApp(App).use(store).use(router).mount('#app')
+new Vue({
+  router,
+  store
+}).$mount('#app')
